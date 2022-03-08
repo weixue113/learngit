3b4ab839e325933a7aefa3928de48f549b80f7ad
Etcrain <xuexue@Macintosh.local>
2022-03-08 10:57:55 +0800

    First Commit

---
+++ git/git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git
+//
+//  Created by Etcrain on 2022/3/8.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
